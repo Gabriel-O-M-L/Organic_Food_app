cdcf2a7e8f90dc312722365d9e6342913f6651d5
Deads_boy <50149984+DEDE3006@users.noreply.github.com>
2022-04-03 10:57:55 -0300

    Delete Backend directory

--- Backend/back/api/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from back import models
-
-class BackSerializers(serializers.ModelSerializer):
-    class Meta:
-        model =  models.Back
-        fields = '__all__'
